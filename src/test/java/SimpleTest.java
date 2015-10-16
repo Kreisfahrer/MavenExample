0a533ca902a5f608586457561d8602898ca4f887
unknown <kreisfahrer@gmail.com>
2015-10-16 20:40:43 +0300

    Added new test method

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -4,17 +4,24 @@ import org.testng.annotations.Test;
 
 public class SimpleTest {
 
-
     @Test(dataProvider = "primalNumbers",dataProviderClass = DataProvider.class, groups = "DDT")
     public void simpleTest(int number,boolean expectedResult){
         PrimalNumberChecker checker = new PrimalNumberChecker();
         boolean result = checker.isNumberPrimal(number);
         Assert.assertEquals(result, expectedResult);
     }
+
     @Test (groups = "sanity")
     public void fifteenTest(){
         PrimalNumberChecker checker = new PrimalNumberChecker();
         boolean result = checker.isNumberPrimal(15);
         Assert.assertEquals(result,false);
+    }
+
+    @Test (groups = "sanity")
+    public void twentyOneTest(){
+        PrimalNumberChecker checker = new PrimalNumberChecker();
+        boolean result = checker.isNumberPrimal(21);
+        Assert.assertEquals(result, false);
     }
 }
